01656911c3547a27dae04567072cd54667883fcf
Ming Qin <qinming.zju@gmail.com>
2016-05-27 23:07:38 -0700

    Finish development
    Yay!

---
+++ README.md
@@ -0,0 +1,11 @@
+# Simple `virtualenv` wrapper
+
+A set of shell functions for activating Python virtualenv. It does exactly the same thing as I used to do by hand on daily basis.
+
+I'm trying to keep this project as small as possible.
+
+Compatible for Mac OS and Linux.
+
+## Installation
+
+## Usage
---
+++ ve.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+#
+# Copyright (c) 2016 Ming Qin (覃明) <https://github.com/QinMing>
+# Open source under MIT LICENSE.
+# You are more than welcome to come together and make this better.
+
+function ve() {
+  local VENV_ROOT="$HOME/virtualenvs/"
+  # Change this to your favorate path
+  # Leave one '/' at the end
+
+  case "$1" in
+    -[lL]|--list|"")
+      echo `find ~/virtualenvs/* -type d -depth 0 | xargs basename`
+      return
+      ;;
+
+    -[dD]|--delete)
+      read -p "rm -r $VENV_ROOT$2 : Is this OK? (y/n)" -n 1 -r
+      echo
+      if [[ $REPLY =~ ^[Yy]$ ]]
+      then
+        rm -r $VENV_ROOT$2
+      fi
+      return
+      ;;
+
+    -[pP]|--root)
+      echo "not implemented yet"
+      return
+      ;;
+
+    -*)
+      echo "Simple virtualenv wrapper for Python"
+      echo
+      echo "Usage:"
+      echo "  ve [option] <name> [additional options]"
+      echo
+      echo "Options:"
+      echo "  without option       Activate virtualenv, or create one if doesn't exist."
+      echo "                       Only when creating new environments, you can add"
+      echo "                       options that will be passed to virtualenv."
+      echo "                       See \`virtualenv -h\`"
+      echo "  -h, --help           Show this info."
+      echo "  -l, --list           List existing virtualenvs (things in VENV_ROOT)."
+      echo "  -p, --root           Change the root path."
+      echo "  -d, --delete <name>  Delete a virtualenv."
+      echo
+      echo "Additional Options"
+      echo "  be passed to virtualenv. Please see \`virtualenv -h\` for details"
+      return
+      ;;
+
+    *)
+      local VENV_PATH=$VENV_ROOT$1
+      local actv=$VENV_PATH/bin/activate
+      shift 1
+      if [ -s $actv ]; then  # virtualenv exists
+        source $actv
+      else                   # virtualenv doesn't exist, let's create one
+        read -p "virtualenv $VENV_PATH $* : Is this OK? (y/n)" -n 1 -r
+        echo
+        if [[ $REPLY =~ ^[Yy]$ ]]
+        then
+          virtualenv $VENV_PATH $*
+        fi
+      fi
+      return
+      ;;
+  esac
+}
